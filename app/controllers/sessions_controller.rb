df40555754d330fbf079687578831332b932de50
Tyler Philbrick <t5j6p9@gmail.com>
2018-06-03 23:35:34 +0000

    Login will now return you to the page you were on before

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,17 @@
 class SessionsController < ApplicationController
   def new
+    session[:login_return] = request.referrer
   end
 
   def create
     user = User.find_by(login_name: params[:sessions][:username])
     if user && user.authenticate(params[:sessions][:password])
       log_in user
-      redirect_to profile_path
+      if session[:login_return] == root_path
+        redirect_to profile_path
+      else
+        redirect_to session[:login_return]
+      end
     else
       flash[:danger] = 'Invalid username/password'
       render 'new'
